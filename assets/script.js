4217af71cd746046dd7785ab3c5387a2cadaf6f8
Taani Maama <taani1maama@live.com>
2020-11-03 22:32:15 -0800

    Added the object in which I am going to store everything in localStorage for.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,31 @@
+// DECLARE OUR VARIABLES
+// HTML Elements
+const notes9amEl = document.querySelector('#9')
+const notes10amEl = document.querySelector('#10')
+const notes11amEl = document.querySelector('#11')
+const notes12pmEl = document.querySelector('#12')
+const notes1pmEl = document.querySelector('#13')
+const notes2pmEl = document.querySelector('#14')
+const notes3pmEl = document.querySelector('#15')
+const notes4pmEl = document.querySelector('#16')
+const notes5pmEl = document.querySelector('#17')
+
+// Notes Object
+let savedNotes = {
+    schedule9am: notes9amEl.value,
+    schedule10am: notes10amEl.value,
+    schedule11am: notes11amEl.value,
+    schedule12pm: notes12pmEl.value,
+    schedule1pm: notes1pmEl.value,
+    schedule2pm: notes2pmEl.value,
+    schedule3pm: notes3pmEl.value,
+    schedule4pm: notes4pmEl.value,
+    schedule5pm: notes5pmEl.value,
+}
+// localStorage
+const savedSchedule = JSON.parse(localStorage.getItem('savedSchedule')) || [];
+
+
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
@@ -9,7 +37,7 @@ $('#currentDay').text(currentDay.format('dddd, MMMM do'));
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 for (let timeIndex = 9; timeIndex <= 17; timeIndex++) {
-    const element = array[timeIndex];
+    const schedule = savedSchedule[timeIndex];
     
 }
 // WHEN I click into a timeblock

7d809e0dc73f19a3d001f2397b862a93dfaa2912
Taani Maama <taani1maama@live.com>
2020-11-04 23:40:13 -0800

    Added placeholders to my textareas.
    Removed a console.log in my js
    
    Almost finished my README, just need to add video.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# 05-Workday-Scheduler
-This is the week 5 homework assignment for the UW Coding Bootcamp.
+# Work Day Scheduler
+
+This is the homework assignment for week 5 of UW Coding Bootcamp. Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS (using Bootstrap) powered by jQuery.
+
+This program is using momentJS for its time elements. 
+
+## Directory
+    1. Visual Example
+    2. User Story
+    3. Criteria
+    4. My Journey
+    5. Resources
+
+## Visual Example
+![Alt Text](https://media.giphy.com/media/bRpOHPIcH5LnxShyEX/giphy.gif)
+
+## User Story
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## My Journey
+```
+The implementation of third-party APIs really streamlined me through this project. I actually did not need to outsource my learning basically at all and just relied on studying the API documentations and trial and error to see what worked. This really helped give me an understanding of not only why the code worked but also why these APIs are so awesome. They really help create some amazing programs with ease with what would otherwise be very daunting. 
+
+The hardest part was getting my textareas to be color coded to the current time and the likes. This took me the longest and also helped me get in the groove of using jQuery the most.
+```
+
+## Resources
+```
+* https://getbootstrap.com/docs/4.1/
+* https://api.jquery.com/
+* https://stackoverflow.com/
+
+* Anthony Brown (Instructor) - gave some great tips for working with localStorage and jQuery I don't think I was going to find anywhere else.
+```
--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,51 @@
 // DECLARE OUR VARIABLES
 const currentDay = moment();
 
-// Empty our textareas so that there are no weird spaces from the page loading.
-$('textarea').empty()
+// WHEN I open the planner the current day is displayed at the top of the calendar
+$('#currentDay').text(currentDay.format('dddd, MMMM do'));
 
 // saveBtn event
 $('.saveBtn').on('click', (e) => {
-    let hourClicked = $(e.target).siblings('textarea').data('time')
-    let notesSaved = $(e.target).siblings('textarea').val()
+    // LET hourClicked equal the buttons clicked sibling textarea's date-time.
+    let hourClicked = $(e.target).prev().data('time')
+    // LET notesSaved equal the buttons clicked sibling textarea's value.
+    let notesSaved = $(e.target).prev().val()
     
-    localStorage.setItem('scheduled-hour-' + hourClicked, notesSaved)
+    localStorage.setItem('hour' + hourClicked, notesSaved)
     console.log(localStorage)
 });
 
 // localStorage
-for (let notesIndex = 9; notesIndex <=17; notesIndex++) {
-const savedSchedule = JSON.stringify(localStorage.getItem('scheduled-hour-' + notesIndex));
-    console.log(savedSchedule)
-    if(savedSchedule === null) {
-        $('textarea').empty()
+// FOR each schedule noted we are placing which text goes where it by a index from 9 to 17.
+for (let notesIndex = 9; notesIndex <= 17; notesIndex++) {
+    // LET 'savedSchedule' GET the values of specified key index.
+    let savedSchedule = localStorage.getItem('hour' + notesIndex);
+    $(`#hour${notesIndex}`).text(savedSchedule)
+    
+    // IF the text is null because there was nothing prev saved then the textarea is emptied.
+    if ($(`#hour${notesIndex}`).text() == null) {
+        $(`#hour${notesIndex}`).text().empty();
     }
+    
 }
-// WHEN I open the planner the current day is displayed at the top of the calendar
-$('#currentDay').text(currentDay.format('dddd, MMMM do'));
 
 // FOR each hour (military time) in the workday we are going to check if it matches the number in our textarea IDs.
 for (let timeIndex = 9; timeIndex <= 17; timeIndex++) {
     // Our const currentTime is equal to the id 'hour' with the matching with the number of timeIndex.
     const currentTime = $(`#hour${timeIndex}`).data('time');
 
+    // IF the data-time from our textarea IDs is less than the current hour...
+    // THEN the past class is added and textarea is grey
     if (parseInt(currentDay.format('H')) > currentTime) {
         $(`#hour${timeIndex}`).addClass('past')
     }
+    // IF the data-time from our textarea IDs is equal to the current hour...
+    // THEN the present class is added and textarea is red
     else if (parseInt(currentDay.format('H')) == currentTime) {
         $(`#hour${timeIndex}`).addClass('present')
     }
+    // IF the data-time from our textarea IDs is greater than the current hour...
+    // THEN the future class is added and textarea is green
     else if (parseInt(currentDay.format('H')) < currentTime) {
         $(`#hour${timeIndex}`).addClass('future')
     }

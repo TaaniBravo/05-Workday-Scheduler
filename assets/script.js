3013018121976bc05d5033020f123cfd19f8167e
Taani Maama <taani1maama@live.com>
2020-11-04 17:35:04 -0800

    Updated the JS and HTML a bit. The JS is still a work in progress but came upon this idea for checking the time. Tried a few other ways that did not work before this commit.

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,52 @@
 // DECLARE OUR VARIABLES
-// HTML Elements
-const notes9amEl = document.querySelector('#9')
-const notes10amEl = document.querySelector('#10')
-const notes11amEl = document.querySelector('#11')
-const notes12pmEl = document.querySelector('#12')
-const notes1pmEl = document.querySelector('#13')
-const notes2pmEl = document.querySelector('#14')
-const notes3pmEl = document.querySelector('#15')
-const notes4pmEl = document.querySelector('#16')
-const notes5pmEl = document.querySelector('#17')
+let timeIndex = 0;
+
+// saveBtn event
+$('.saveBtn').on('click', handleSchedule);
 
 // Notes Object
 let savedNotes = {
-    schedule9am: notes9amEl.value,
-    schedule10am: notes10amEl.value,
-    schedule11am: notes11amEl.value,
-    schedule12pm: notes12pmEl.value,
-    schedule1pm: notes1pmEl.value,
-    schedule2pm: notes2pmEl.value,
-    schedule3pm: notes3pmEl.value,
-    schedule4pm: notes4pmEl.value,
-    schedule5pm: notes5pmEl.value,
+    schedule9am: $('#hour9').value,
+    schedule10am: $('#hour10').value,
+    schedule11am: $('#hour11').value,
+    schedule12pm: $('#hour12').value,
+    schedule1pm: $('#hour13').value,
+    schedule2pm: $('#hour14').value,
+    schedule3pm: $('#hour15').value,
+    schedule4pm: $('#hour16').value,
+    schedule5pm: $('#hour17').value,
 }
+
 // localStorage
 const savedSchedule = JSON.parse(localStorage.getItem('savedSchedule')) || [];
-
+savedSchedule.push(savedNotes)
 
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-var currentDay = moment();
+const currentDay = moment();
 $('#currentDay').text(currentDay.format('dddd, MMMM do'));
+console.log(parseInt(currentDay.format('H')))
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-for (let timeIndex = 9; timeIndex <= 17; timeIndex++) {
-    const schedule = savedSchedule[timeIndex];
-    
-}
-// WHEN I click into a timeblock
-// THEN I can enter an event
+$('textarea').each(function() {
+    if (parseInt(currentDay.format('H')) > $('textarea').data('time') ) {
+        $('textarea').addClass('past')
+    }
+    else if (parseInt(currentDay.format('H')) == $('textarea').data('time') ) {
+        $('textarea').addClass('present')
+    }
+    else if (parseInt(currentDay.format('H')) < $('textarea').data('time') ) {
+        $('textarea').addClass('future')
+    }
+})
+
+// console.log(parseInt(currentDay.format('H')))
+
 // WHEN I click the save button for that timeblock
+function handleSchedule() {
+
+}
+
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
